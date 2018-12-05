7153c5c7910d75ee252c585a50059124a42b200e
annabelkrijgsman <annabelkrijgsman@gmail.com>
2018-12-05 12:12:43 +0100

    Small changes in style and function

--- blog.js
+++ blog.js
@@ -1,6 +1,6 @@
 //SELECT CATEGORIES
 function selectCategory() {
     
-    document.location.href = "main.php?cat=" + document.getElementById("selectcategory").value;
+    document.location.href = "/blog/main.php?cat=" + document.getElementById("selectcategory").value;
     
 }
