38e0314747a57180cbe10d4d6ebe07ed887b4a97
annabelkrijgsman <annabelkrijgsman@gmail.com>
2018-12-06 16:11:52 +0100

    Added portfolio projects

--- aside.php
+++ aside.php
@@ -1,7 +1,7 @@
 <div class="personal">
-
-    <img alt="" class="img-circle" widht="200px" height="230px" src="https://i.imgur.com/d0x1Ip5.jpg">
-         
+    <!--about me blokje + skills + social media-->
+     <img alt="" class="img-circle" widht="200px" height="230px" src="https://i.imgur.com/d0x1Ip5.jpg">
+     
         <h2>Introduction</h2>
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
             Aenean commodo ligula eget dolor.
@@ -9,7 +9,6 @@
             Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.<br/>
             <br/>
             <hr/>
-            
         <h2>Skills</h2>
             <img class="icon" src="https://i.imgur.com/hBkkjFo.png" alt=""/>
             <img class="icon" src="https://i.imgur.com/7alj1xg.png" alt=""/>
@@ -17,18 +16,15 @@
             <img class="icon" src="https://i.imgur.com/TAua3Ny.png" alt=""/>
             <br/>
             <hr/>
-            
         <h2>Portfolio Projects</h2>
-            <ul id="marker">
-                <li><a href="https://www.google.com/" target="_blank">Project 1</a></li>
-                <br/>
-                <li><a href="https://www.google.com/" target="_blank">Project 2</a></li>
-                </br>
-                <li><a href="https://www.google.com/" target="_blank">Project 3</a></li>
+            <ul>
+                <li><a href="#">Project 1</a></li>
+                <li><a href="#">Project 2</a></li>
+                <li><a href="#">Project 3</a></li>
             </ul>
             <br/>
             <hr/>
-                    
+            
     <div class="socialicons">
         <h2>Social Media</h2>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -38,5 +34,4 @@
                <a href="https://www.youtube.com" target="_blank" class="fa fa-youtube"></a>
                <a href="https://www.pinterest.com" target="_blank" class="fa fa-pinterest"></a>
     </div>
-            
 </div>
