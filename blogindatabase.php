044a90c2e02124a062dee67579df768cc07c19fc
annabelkrijgsman <annabelkrijgsman@gmail.com>
2018-12-07 12:21:19 +0100

    Cleaned up code

--- blogindatabase.php
+++ blogindatabase.php
@@ -1,44 +1,35 @@
 <?php
-//FROM FIELD INTO DATABASE
 
-		$username = "";
-		$title = "";
-		$post = "";
-		$date = "";
-		$catname = "";
+//FORM INTO DATABASE
+$username = "";
+$title = "";
+$post = "";
+$date = "";
+$catname = "";
 	
 if ($_SERVER["REQUEST_METHOD"] == 'POST' && !empty($_POST["name"]) && !empty($_POST["title"]) && !empty($_POST["subject"]) && !empty($_POST["catname"])) {
-
-		$username = $_POST["name"];
-		$title = $_POST["title"];
-		$post = $_POST["subject"];
-		$date = date("d-m-Y, H:i");
-		$catname = $_POST["catname"];
 		
-		//$sql = "INSERT INTO Blogposts (Username, Title, Post, date) " .
-		//"VALUES ('$username', '$title', '$post', '$date');";
-		
-		$sql = "INSERT INTO Blogposts (Username, Title, Post, date) VALUES ('$username', '$title', '$post', '$date')";
+	$username = $_POST["name"];
+	$title = $_POST["title"];
+	$post = $_POST["subject"];
+	$date = date("d-m-Y, H:i");
+	$catname = $_POST["catname"];
+	
+	$sql = "INSERT INTO Blogposts (Username, Title, Post, date) VALUES ('$username', '$title', '$post', '$date')";
+	mysqli_query($conn, $sql);
+	
+	//CATEGORIES INTO DATABASE
+	$blogID = mysqli_insert_id($conn);
+	
+	$cname = "";
+	
+	for ($i=0; $i<count($catname); $i++) {
+		$sql = "INSERT INTO Blogposts_Categories (BlogpostID, CategoryID) VALUES ($blogID, $catname[$i])";
 		mysqli_query($conn, $sql);
-   
-		//$connection->exec($sql);
-		
-		$blogID = mysqli_insert_id($conn);
-		//$blogID = $connection->lastInsertId();
-		
-		$cname = "";
-		
-		for ($i=0; $i<count($catname); $i++) {
-			$sql = "INSERT INTO Blogposts_Categories (BlogpostID, CategoryID) VALUES ($blogID, $catname[$i])";
-			mysqli_query($conn, $sql);
-			
-			
-			//$sql = "INSERT INTO Blogposts_Categories (BlogpostID, CategoryID) " .
-			//"VALUES ($blogID, $catname[$i])";
-			//$connection->exec($sql);
-		}
-		
-		echo "<script>document.location.href='editblog.php?blogindb=yes';</script>"; die();
-		
 	}
+	
+	echo "<script>document.location.href='loginsignup.php?blogindb=yes';</script>"; die();
+	
+}
+
 ?>
