ed30dc78b004192322b0a5dd0252d9c2f90bb51d
annabelkrijgsman <annabelkrijgsman@gmail.com>
2018-12-07 12:19:08 +0100

    Cleaned up code

--- main.php
+++ main.php
@@ -9,9 +9,10 @@
               
             <select id="selectcategory" class="button" onchange="selectCategory();">
                 <option value=''>Select Category</option>
-        
+                    
                     <?php
                         
+                        //CATEGORY FROM DATABASE INTO SELECT FORM
                         if (isset($_GET["cat"])) {
                             $catname = $_GET["cat"];
                         }
@@ -30,7 +31,7 @@
                                 if ($catname == $row["ID"]) {
                                     $selected = "selected='selected'";
                                 }
-                                echo "<option value =" . $row['ID'] . " " . $selected . "> " . $row["Catname"] . " </option> ";
+                                echo '<option value =' . $row["ID"] . ' ' . $selected . '> ' . $row["Catname"] . ' </option> ';
                             }
                         }
                      
@@ -42,65 +43,66 @@
                 
                 <input class="searchbox" type="text" name="search" placeholder="Search">    
                 <button type="sumbit" name="submitsearch" class="button">Search</button>
-            
-            <?php
-            
-                if (isset($_POST['submitsearch'])) {
-                    $search = $_POST["search"];
-                    $sql = "SELECT * FROM Blogposts
-                            WHERE Title LIKE '%$search%'
-                            OR Post LIKE '%$search%'
-                            OR Username LIKE '%$search%'
-                            OR Date LIKE '%$search%'";
-
-                    $result = mysqli_query($conn, $sql);
-                    $row = mysqli_fetch_assoc($result);
-                    
-                        foreach ($result as $row) {
-                            echo "<div class='articles'>
-                                <h4 style='color:green'>Your search results</h4>
-                                <h3>" . $row['Title'] . "</h3>
-                                <p><small>" . $row['Date'] . "</small></p>
-                                <a href='readblog.php?id=" . $row["ID"] . "'>Read article..</a>
-                                </div>";
-                        }   
-                }
                 
-            ?>
+                    <?php
+                        
+                        //SEARCH FIELD
+                        if (isset($_POST['submitsearch'])) {
+                            $search = $_POST["search"];
+                            $sql = "SELECT * FROM Blogposts
+                                    WHERE Title LIKE '%$search%'
+                                    OR Post LIKE '%$search%'
+                                    OR Username LIKE '%$search%'
+                                    OR Date LIKE '%$search%'";
+                              
+                            $result = mysqli_query($conn, $sql);
+                            $row = mysqli_fetch_assoc($result);
+                            
+                                foreach ($result as $row) {
+                                    echo '<div class="articles">';
+                                    echo '<h4 style="color:green">Your search results</h4>';
+                                    echo '<h3>' . $row["Title"] . '</h3>';
+                                    echo '<p><small>' . $row["Date"] . '</small></p>';
+                                    echo '<a href="readblog.php?id=' . $row["ID"] . '">Read article..</a>';
+                                    echo '</div>';
+                                }   
+                        }
+                        
+                    ?>
                 
             </form>
             <br/>								
             <hr/>
             <br/>
-
-                <?php
-                    //SELECT ARTICLES FROM DATABASE                          
-                    if (strlen($catname) > 0) {
-                        $sql = "SELECT * FROM Blogposts
-                            LEFT JOIN Blogposts_Categories
-                            ON Blogposts.ID = Blogposts_Categories.BlogpostID
-                            WHERE Blogposts_Categories.CategoryID = '$catname'
-                            ORDER BY date DESC" or die;
-                    }
-                    else {
-                        $sql = "SELECT * FROM Blogposts ORDER BY date DESC" or die;
-                        
-                    }
-                    
-                    $result = mysqli_query($conn, $sql);
-                    $resultcheck = mysqli_num_rows($result);
-                    
-                    if ($resultcheck > 0) {
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            echo "<div class='artcilebox'>
-                                <h3>" . $row['Title'] . "</a></h3>
-                                <p><small>" . $row['Date'] . "</small></p>
-                                <a href='readblog.php?id=" . $row["ID"] . "'>Read article..</a>
-                                </div><hr/>";
+            
+                    <?php
+                        //SELECT ARTICLES FROM DATABASE                          
+                        if (strlen($catname) > 0) {
+                            $sql = "SELECT * FROM Blogposts
+                                LEFT JOIN Blogposts_Categories
+                                ON Blogposts.ID = Blogposts_Categories.BlogpostID
+                                WHERE Blogposts_Categories.CategoryID = '$catname'
+                                ORDER BY date DESC" or die;
                         }
-                    }
-                    
-                ?>
+                        else {
+                            $sql = "SELECT * FROM Blogposts ORDER BY date DESC" or die;
+                            
+                        }
+                        
+                        $result = mysqli_query($conn, $sql);
+                        $resultcheck = mysqli_num_rows($result);
+                        
+                        if ($resultcheck > 0) {
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                echo '<div class="articlebox">';
+                                echo '<h3>' . $row["Title"] . '</a></h3>';
+                                echo '<p><small>' . $row["Date"] . '</small></p>';
+                                echo '<a href="readblog.php?id=' . $row["ID"] . '">Read article..';
+                                echo '</div><hr/>';
+                            }
+                        }
+                        
+                    ?>
                 
                 
         </div>
