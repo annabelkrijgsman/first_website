5448678aaf73be6488e90f62a12a24d2af4c8a2d
annabelkrijgsman <annabelkrijgsman@gmail.com>
2018-12-07 11:46:02 +0100

    Cleaned up code

--- loginsignup.php
+++ loginsignup.php
@@ -1,5 +1,8 @@
 <?php
     include 'headermysqli.php';
+    //echo "<pre>";
+    //var_dump($_SESSION);
+    //echo "</pre>";
 ?>
 
 <br/>
@@ -8,7 +11,8 @@
         <div class="left">
             
             <?php
-                //SIGN UP ERRORS WORKS
+            
+                //SIGN UP ERRORS
                 if (isset($_GET['error'])) {
                     if ($_GET['error'] == "emptyfields") {
                         echo '<p style="color:red">Please fill in all fields</p>';
@@ -29,71 +33,55 @@
                         echo '<p style="color:red">Username is already taken</p>';
                     }
                 }
-                //NOTICE
-                    elseif ($_GET["signup"] == "success") {
+                
+                    elseif (isset($_GET['signup']) && $_GET["signup"] == "success") {
                         echo '<p style="color:green">Thanks for signing up, please login</p>';
                     }
                 
-            ?>
-            
-            <?php
-                //ARTICLE TABLE WORKS
+                //ARTICLE TABLE
                 if (isset($_SESSION['userID'])) {
-                    echo '<h2 align="center">Articles</h2>';	
-            
-                        echo "<table>";
+                    echo '<h2 align="center">Articles</h2>';
+                    echo '<table>';   
+                    echo '<thead>';
+                    echo '<tr>';
+                    echo '<th>Username</th>';
+                    echo '<th>Title</th>';
+                    echo '<th>Post</th>';
+                    echo '<th>Date</th>';
+                    echo '<th>Action</th>';
+                    echo '</tr>';
+                    echo '</thead>';
                             
-                        echo "<thead>";
-                        echo "<tr>";
-                        echo "<th>Username</th>";
-                        echo "<th>Title</th>";
-                        echo "<th>Post</th>";
-                        echo "<th>Date</th>";
-                        echo "<th>Action</th>";
-                        echo "</tr>";
-                        echo "</thead>";
-                            
-                            $sql = "SELECT * FROM Blogposts ORDER BY date DESC";
-                            
-                            $result = mysqli_query($conn, $sql);
-                            foreach ($result as $row) {
-                        
-                                echo "<tbody>";        
-                                echo "<tr>";
-                                echo "<td>" . $row['Username'] . "</td>";
-                                echo "<td>" . $row['Title'] . "</td>";
-                                echo "<td class='post'>" . $row['Post'] . "</td>";
-                                echo "<td>" . $row['Date'] . "</td>";
-                                echo "<td><a class='button' href='editpost.php?id=" . $row["ID"] . "'>Edit</a>  <a class='button' href='editpost.php?id=" . $row["ID"] . "&delete=" . $row["ID"] . "'>Delete</a></td>";
-                                echo "</tr>";
-                                echo "</tbody>";
-                            
-                            }
-                        
-                        echo "</table>";
-                        echo "</table>";
-                }
-                //LOGIN WORKS
-                else {
-                    echo '<h2 align="center">Login</h2>
+                $sql = "SELECT * FROM Blogposts ORDER BY date DESC";
                     
-                        <form method="post" id="contactblock" action="login.php">
-                        
-                            <label for="uname">Username</label><br/>
-                            <input type="text" class="textfield" name="email" placeholder="Username..">
-                            <br/>
-                            <label for="psw">Password</label><br/>
-                            <input type="password" class="textfield" name="pwd" placeholder="Password..">
-                            <br/>
-                            <button type="submit" name="loginsubmit" class="button">Login</button>
-                            
-                        </form>';
+                $result = mysqli_query($conn, $sql);
+                    foreach ($result as $row) {
+                        echo '<tbody>';        
+                        echo '<tr>';
+                        echo '<td>' . $row['Username'] . '</td>';
+                        echo '<td>' . $row['Title'] . '</td>';
+                        echo '<td class="post">' . $row['Post'] . '</td>';
+                        echo '<td>' . $row["Date"] . '</td>';
+                        echo '<td><a class="button" href="editpost.php?id=' . $row["ID"] . '">Edit</a>  <a class="button" href="editpost.php?id=' . $row["ID"] . '&delete=' . $row["ID"] . '">Delete</a></td>';
+                        echo '</tr>';
+                        echo '</tbody>';
+                    }
+                        echo '</table>';
                 }
-            ?>
+                
+                //LOGIN
+                else {
+                    echo '<h2 align="center">Login</h2>';
+                    echo '<form method="post" id="contactblock" action="login.php">';
+                    echo '<label for="uname">Username</label><br/>';
+                    echo '<input type="text" class="textfield" name="email" placeholder="Username.."><br/>';    
+                    echo '<label for="psw">Password</label><br/>';        
+                    echo '<input type="password" class="textfield" name="pwd" placeholder="Password.."><br/>';
+                    echo '<button type="submit" name="loginsubmit" class="button">Login</button>';
+                    echo '</form>';
+                }
             
-            <?php
-            
-                //ADD NEW CATEGORY WORKS
+                //ADD NEW CATEGORY 
                 $category = "";
                     
                 if ($_SERVER["REQUEST_METHOD"] == 'POST' && !empty($_POST["newcat"])) {	
@@ -106,108 +94,85 @@
                     echo "<script>document.location.href='loginsignup.php?catsave=yes';</script>"; die();
                 }
             
-            ?>
-            
-            <?php
-            
                 include_once 'requiredfields.php';
-                //include_once 'blogindatabaseedit.php'; = KAPOT
             
-                //WRITE ARTICLE = KAPOT
+                //ADD NEW CATEGORY FORM
                 if (isset($_SESSION['userID'])) {
-                        echo '<h2 align="center">Write artcile</h2>
+                        echo '<h2 align="center">Write artcile</h2>';
+                        echo '<form method="post" id="contactblock" action="">';
+                        echo '<input type="text" class="textfield" name="newcat" placeholder="Add a new category.." value="">';
+                        echo '<input type="submit" class= "button" value="Save"><br/>';
+                        echo '</form>';
+                        echo '<p>Choose article category:</p>';
+                        echo '<form method="post" id="contactblock" action="">';
                         
-                                <form method="post" id="contactblock" action="">
+                //SELECT CATEGORY                
+                    $sql = "SELECT * FROM Categories";
                                 
-                                    <input type="text" class="textfield" name="newcat" placeholder="Add a new category.." value="">
-                                    <input type="submit" class= "button" value="Save"><br/>
-                                    
-                                </form>
-                                
-                                <p>Choose article category</p>
-                                <br/>
-                                <br/>';
-                                
-            
-            
-                                //CATEGORIES FROM DATABASE TO CHECKBOXES = KAPOT
-                                $sql = "SELECT * FROM Categories";
-                                            
-                                $result = mysqli_query($conn, $sql);
-                                foreach ($result as $row) {
-                                    
-                                    $checked = "";
-                                   //NOTICE                        
-                                    if (in_array($row["id"], $categoryIDs)) {
-                                        $checked = "checked";
-                                    }
-                                    
-                                    echo "<input type='checkbox' name='catname[]' value=" . $row["ID"] . " " . $checked . "> " . $row["Catname"] . " <br/> ";
-                                }
-            ?>
-            
-            
-                                <br/>
-                            <div class="contactcolumn">
-            
-                    
-                                <form method="post" id="contactblock" action="">
+                    $result = mysqli_query($conn, $sql);
+                    foreach ($result as $row) {
                         
-                                    <label for="fname">Username</label><br/>
-            
-                                    <input type="text" class="textfield" name="name" placeholder="Your username.." value="<?php echo $name;?>">
-                                    <span class="error"><b>* </b><?php echo $nameErr;?></span>
-                                    <br/>
-                                    <br/>
-                                    <label for="title">Article title</label><br/>
-                                    <input type="text" class="textfield" name="title" placeholder="Your article title.." value="<?php echo $title;?>">
-                                    <span class="error"><b>* </b> <?php echo $titleErr;?></span>	
-                                    <br/>
-                                    <br/>
-                                    <label for="article">Article</label><br/>
-                                    <textarea class="textfield" name="subject" rows="10" cols="40" placeholder="Write your article.."><?php echo $comment;?></textarea>
-                                    <span class="error"><b>* </b> <?php echo $commentErr;?></span>
-            <?php                            
-                        echo           '<p>
-                                            <span class="error">
-                                                <b>* required fields</b>
-                                            </span>
-                                        </p>
-                                        
-                                        <input type="submit" class= "button" value="Save"><br/>
-                                </form>                
-                            </div>';
-            
-                }
-
-                //SIGN UP WORKS
-                else {
-                    echo '<h2 align="center">Sign Up</h2>
-            
-                                <form method="post" id="contactblock" action="signup.php">
-                                
-                                    <label for="uname">Username</label><br/>
-                                    <input type="text" id="uname" class="textfield" name="uname" placeholder="Username..">
-                                    <br/>
-                                    <label for="email">Email</label><br/>
-                                    <input type="text" id="email" class="textfield" name="email" placeholder="Email..">
-                                    <br/>
-                                    <label for="psw">Password</label><br/>
-                                    <input type="password" id="pwd" class="textfield" name="pwd" placeholder="Password..">	
-                                    <br/>
-                                    <label for="psw">Password</label><br/>
-                                    <input type="password" id="pwdrepeat" class="textfield" name="pwdrepeat" placeholder="Repeat password..">	
-                                    <br/>
-                                    
-                                    <button type="submit" name="signupsubmit" class="button">Sign Up</button>
-                                    
-                                </form>';
+                        echo "<input type='checkbox' name='catname[]' value=" . $row["ID"] . "> " . $row["Catname"] . " <br/> ";
                     }
                 
-                ?>
+            ?>
+                <!--WRITE ARTICLE-->
+                    <br/>
+                    <div class="contactcolumn">
+                    
+                    <label for="fname">Username</label><br/>
+                    <input type="text" class="textfield" name="name" placeholder="Your username.." value="<?php echo $name;?>">
+                    <span class="error"><b>* </b><?php echo $nameErr;?></span><br/><br/>
+                    <label for="title">Article title</label><br/>
+                    <input type="text" class="textfield" name="title" placeholder="Your article title.." value="<?php echo $title;?>">
+                    <span class="error"><b>* </b> <?php echo $titleErr;?></span><br/><br/>
+                    <label for="article">Article</label><br/>
+                    <textarea class="textfield" name="subject" rows="10" cols="40" placeholder="Write your article.."><?php echo $comment;?></textarea>
+                    <span class="error"><b>* </b> <?php echo $commentErr;?></span>
+                                    
+            <?php                            
+                
+                    echo '<span class="error">';
+                    echo '<p><b>* required fields</b></p>';
+                    echo '</span>';
+                    echo '<input type="submit" class= "button" value="Save"><br/>';
+                    echo '</form>';
+                    echo '</div>';
             
-                <?php
-                    //include 'blogindatabase.php'; = KAPOT
+                }
+                
+                //SIGN UP
+                else {
+                    echo '<h2 align="center">Sign Up</h2>';
+                    echo '<form method="post" id="contactblock" action="signup.php">';
+                    echo '<label for="uname">Username</label><br/>';
+                    echo '<input type="text" id="uname" class="textfield" name="uname" placeholder="Username.."><br/>';
+                    echo '<label for="email">Email</label><br/>';
+                    echo '<input type="text" id="email" class="textfield" name="email" placeholder="Email.."><br/>';
+                    echo '<label for="psw">Password</label><br/>';
+                    echo '<input type="password" id="pwd" class="textfield" name="pwd" placeholder="Password.."><br/>';
+                    echo '<label for="psw">Repeat password</label><br/>';
+                    echo '<input type="password" id="pwdrepeat" class="textfield" name="pwdrepeat" placeholder="Repeat password.."><br/>';
+                    echo '<select name="groupname" class="button">';
+                    echo '<option value="">Select group</option>';
+                 
+                    //SELECT USERGROUP   
+                    $sql = 'SELECT * FROM Usergroups';
+                    $result = mysqli_query($conn, $sql);
+                    
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo '<option value=' . $row["ID"] . '>' . $row["Groupname"] . '</option>';
+                    }
+                                    
+                                    
+                    echo '</select><br/>';
+                    echo '<button type="submit" name="signupsubmit" class="button">Sign Up</button>';
+                    echo '</form>';
+                }
+                
+                
+                include 'blogindatabase.php';
+                
                 ?>
         </div>
     </article>
